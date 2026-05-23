381022637fd3fc6125449e7ac1abf8063d31fd0a
oxcm07 <oxcm0727@gmail.com>
2026-05-23 18:06:51 +0900

    3-00

---
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -0,0 +1,40 @@
+package com.mysite.sbb.question;
+
+import com.mysite.sbb.answer.Answer;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Setter
+@Getter
+public class Question {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private int id;
+
+    private LocalDateTime createDate;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @OneToMany(mappedBy = "question", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<Answer> answerList = new ArrayList<>();
+
+    public void addAnswer(String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setQuestion(this);
+        answer.setCreateDate(LocalDateTime.now());
+        answerList.add(answer);
+    }
+}
